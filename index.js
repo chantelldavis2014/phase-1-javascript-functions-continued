c645edfb79f73214135b57e927aaaa743f04353d
Chantell Davis <chantelldavis2014@yahoo.com>
2022-11-25 23:36:33 -0500

    commit tree 628a5003594a753cee384d82fd92993e197b9113

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+    }
+    saturdayFun()
+
+function mondayWork(work="go to the office"){
+    return `This Monday, I will ${work}.` 
+   }
+   mondayWork()
+   
+   function wrapAdjective (star="*") {
+    return function(adj="Beauiful"){
+      return `You are ${star}${adj}${star}!`
+    }
+  }
